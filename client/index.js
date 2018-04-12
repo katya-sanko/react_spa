b66219161e32a8e833bd1958270d8e629a69965a
Sanko, Ekaterina <ekaterina.sanko@kronos.com>
2018-04-12 19:24:53 +0300

    Rendering of first component

--- client/index.js
+++ client/index.js
@@ -5,3 +5,28 @@ ReactDOM.render(
     <h1>Hello, world!</h1>,
     document.getElementById('root')
 );
+
+// class TestClass1 extends React.Component {
+//     render () {
+//         return (
+//             React.createElement("div",
+//                 { className: "container" },
+//                 React.createElement("h1", null, "React Component example")
+//                 )
+//         );
+//     }
+// }
+//
+// ReactDOM.render(React.createElement(TestClass1, null), document.getElementById('root'));
+
+class TestClass2 extends React.Component {
+    render () {
+        return (
+           <div className="container">
+               <h1>Hello, {this.props.name}</h1>
+           </div>
+        );
+    }
+}
+
+ReactDOM.render(<TestClass2 name="Noname"/>, document.getElementById('root'));
