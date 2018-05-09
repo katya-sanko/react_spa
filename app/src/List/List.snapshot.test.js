d6ba4cd36e61d828c6305e7152779d84a0123ea4
katya <->
2018-05-09 20:41:26 +0300

    Added enzyme test, deleted unused files, updated package.json

---
+++ app/src/List/List.snapshot.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import List from './List';
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+    const tree = renderer
+        .create(<List />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
--- app/src/testFunc.test.js
+++
@@ -1,5 +0,0 @@
-const testFunc = require('./testFunc');
-
-test('tests testFunc', () => {
-    expect(testFunc(1, 2)).toBe(3);
-});
