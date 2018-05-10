f3d0d34bafaa775c8a1d569c2dd66bcfa3b71501
katya <->
2018-05-10 08:58:35 +0300

    Added movies mock

--- app/src/List/List.snapshot.test.js
+++ app/src/List/List.snapshot.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import List from './List';
 import renderer from 'react-test-renderer';
 
+const mockMovies = [];
+
 it('renders correctly', () => {
     const tree = renderer
-        .create(<List />)
+        .create(<List movies={mockMovies}/>)
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
--- app/src/List/List.test.js
+++ app/src/List/List.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import List from './List';
 
+const mockMovies = [];
+
 it('should return a <div />', () => {
-    const tree = shallow(<List />);
+    const tree = shallow(<List movies={mockMovies}/>);
     expect(tree.type()).toEqual('div');
 });
