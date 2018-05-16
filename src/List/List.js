d65ffc0ad3ab37081619be85980a49d4bd0b5b2d
katya <->
2018-05-16 21:17:28 +0300

    Clean up List

--- src/List/List.js
+++ src/List/List.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../Card/Card';
 import './list.css';
 
-const List = ({ movies }) => (
-    <div className="list-wrapper">
-        { !movies.length && <span>No films found</span> }
-        <ul className="list-container">
-            {
-                movies && movies.map((movie, index) => <li className="card-wrapper" key={index}>  <Card movie={movie} />  </li>)
-            }
-        </ul>
-    </div>
+class List extends Component {
+    constructor(props) {
+        super(props);
 
-);
+        this.state = {
+            movies: this.props.movies,
+            hasErrored: false,
+            isLoading: false
+        };
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+            movies: nextProps.movies,
+            hasErrored: !nextProps.movies.length,
+        };
+    }
+
+    render() {
+        if (this.state.hasErrored) {
+            return <span>No films found</span>
+        }
+
+        if (this.state.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <div className="list-wrapper">
+                <ul className="list-container">
+                    {
+                        this.state.movies && this.state.movies.map((movie, index) => <li className="card-wrapper" key={index}>  <Card movie={movie} />  </li>)
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default List;
