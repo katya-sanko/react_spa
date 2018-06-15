23315b5d950f363604c11598d65a3bfc207c7734
Sanko, Ekaterina <ekaterina_sanko@epam.com>
2018-06-15 13:11:19 +0300

    Draft of app rearrangement for a SSR

--- server/app.js
+++ server/app.js
@@ -4,21 +4,23 @@ const app = express();
 
 if (process.env.NODE_ENV === 'development') {
     const webpack = require('webpack');
-    const webpackConfig = require('./webpack.dev.js');
+    // const webpackConfig = require('./webpack.dev.js');
+    const webpackConfig = require('../webpack.dev.js');
     const webpackDevMiddleware = require('webpack-dev-middleware');
     const webpackHotMiddleware = require('webpack-hot-middleware');
+    const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
+
     const compiler = webpack(webpackConfig);
 
     app.use(webpackDevMiddleware(compiler));
-    app.use(webpackHotMiddleware(compiler));
+    app.use(webpackHotMiddleware(compiler.compilers.find(c => c.name === 'client')));
+    app.use(webpackHotServerMiddleware(compiler));
 
 } else {
+    const serverRenderer = require('../dist/js/serverRenderer').default;
+
     app.use(express.static(path.join(__dirname, '/dist')));
+    app.use(serverRenderer());
 }
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-
-app.listen(3002, () => console.log('Example app listening on port 3002!'));
+module.exports = app;
